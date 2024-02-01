c4b04fc2fbc2d247afb67bc7a9e4406744cf90c7
GodwinObande <onahgodwinobande@gmail.com>
2024-02-01 04:05:56 +0000

    Adding Bagshop items to heroku account

--- API/ApiErrorMiddleWares/ErrorMiddleWare.cs
+++ API/ApiErrorMiddleWares/ErrorMiddleWare.cs
@@ -2,7 +2,6 @@ using System.Net;
 using System.Text.Json;
 using API.ErrorsHandlers;
 
-
 namespace API.ApiErrorMiddleWares
 {
     public class ErrorMiddleWare
@@ -10,43 +9,41 @@ namespace API.ApiErrorMiddleWares
         private readonly RequestDelegate _next;
         private readonly ILogger _iloger;
         private readonly IHostEnvironment _env;
-        public ErrorMiddleWare(RequestDelegate next, ILogger<ErrorMiddleWare> iloger, IHostEnvironment env)
+        public ErrorMiddleWare(RequestDelegate next, ILogger < ErrorMiddleWare > iloger, IHostEnvironment env)
         {
             _env = env;
             _iloger = iloger;
             _next = next;
         }
 
-        public async Task InvokeAsync(HttpContext httpContext){
+        public async Task InvokeAsync(HttpContext httpContext) {
 
-                try{
-                          await  _next(httpContext);
+            try {
+                await _next(httpContext);
 
-                }
+            } catch (Exception e) {
 
-                catch(Exception e){
+                _iloger.LogError(e, e.Message);
 
-                       _iloger.LogError(e, e.Message);
+                httpContext.Response.ContentType = "application/json";
 
-                       httpContext.Response.ContentType = "application/json";
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                       httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                var response = _env.IsDevelopment()
+                    ? new ApiErrorExceptions((int)HttpStatusCode.InternalServerError, e.Message, e.StackTrace.ToString())
+                    : new ApiErrorExceptions((int)HttpStatusCode.InternalServerError);
 
-                       var response = _env.IsDevelopment() ? new ApiErrorExceptions(
-                                    (int)HttpStatusCode.InternalServerError, e.Message, 
-                                                 e.StackTrace.ToString()): 
-                                                 new ApiErrorExceptions(
-                                                    (int)HttpStatusCode.InternalServerError);
+                var options = new JsonSerializerOptions {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
 
-                        var options = new JsonSerializerOptions{PropertyNamingPolicy =
-                                     JsonNamingPolicy.CamelCase};
+                var json = JsonSerializer.Serialize(response, options);
 
-                        var json = JsonSerializer.Serialize(response,options);
+                await httpContext
+                    .Response
+                    .WriteAsync(json);
 
-                        await httpContext.Response.WriteAsync(json);
-
-
-                }
+            }
         }
     }
 }
